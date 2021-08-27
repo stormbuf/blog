4964726623346a1c8344f5f8176995f2d500a549
stormbuf <stormbuf@163.com>
2021-08-27 17:22:53 +0800

    add timeLine

--- docs/.vuepress/util/index.js
+++ docs/.vuepress/util/index.js
@@ -270,7 +270,7 @@ export function zero (d) {
 
 // 获取时间的时间戳
 export function getTimeNum (post) {
-  let dateStr = post.frontmatter.date || post.lastUpdated || new Date()
+  let dateStr = post.lastUpdated
   let date = new Date(dateStr)
   if (date == "Invalid Date" && dateStr) { // 修复new Date()在Safari下出现Invalid Date的问题
     date = new Date(dateStr.replace(/-/g, '/'))
