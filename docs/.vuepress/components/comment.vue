3721b476f45642f17fba1fdfeaf8b07d5a5e0d8b
stormbuf <stormbuf@163.com>
2022-01-19 01:08:32 +0800

    ✨ feat: add comment

---
+++ docs/.vuepress/components/comment.vue
@@ -0,0 +1,20 @@
+<template>
+  <Comment :author="'stormbuf'" :content="'just for test'"></Comment>
+</template>
+
+
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Comment } from "ant-design-vue";
+import 'ant-design-vue/dist/antd.css';
+export default defineComponent({
+  components: {
+    Comment,
+  },
+});
+</script>
+
+<style>
+</style>
--- docs/HelloWorld.md
+++ docs/HelloWorld.md
@@ -2,3 +2,5 @@
 hello world!
 
 I'm fine.
+
+<comment></comment>
