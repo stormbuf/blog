c5618cf3766958260a0bab741f8ca3c49c8ae0bc
stormbuf <stormbuf@163.com>
2022-01-18 22:52:44 +0800

    ✨ feat: add baidu statistics

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -17,6 +17,9 @@ export default defineUserConfig<DefaultThemeOptions>({
       rel: 'manifest',
       href: '/manifest.json'
     }],
+    ['script', {},
+      "var _hmt=_hmt||[];(function(){var hm=document.createElement(\"script\");hm.src=\"https://hm.baidu.com/hm.js?36884888f7a05e80a42de77a45873e51\";var s=document.getElementsByTagName(\"script\")[0];s.parentNode.insertBefore(hm,s)})();"
+    ],
 
   ],
   title: 'stormbuf\'s blog',
